3fa5c3cf1e6bc8722ba6d5bc6b73447654c7834f
Mike DelGaudio <mdelgaud@stevens.edu>
2020-05-09 14:51:54 -0400

    adding resume to footer on mobike and css for call to action

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { CallToActionButtonsComponent } from "./call-to-action-buttons/call-to-action-buttons.component";
 
 const routes: Routes = [
-  {path: "", component: HomePageComponent},
+  { path: "", component: HomePageComponent },
+  { path: "test", component: CallToActionButtonsComponent },
+  { path: "**", component: HomePageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

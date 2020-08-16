c8ac946724e4bf5689610b9907af007a1a643232
Mike DelGaudio <mdelgaud@stevens.edu>
2020-08-16 19:31:08 -0400

    initalize custom error handler as well as 404s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { HomePageComponent } from "./home-page/home-page.component";
 import { CreativeWorkPageComponent } from "./creative-work-page/creative-work-page.component";
 import { CodeProjectsPageComponent } from "./code-projects-page/code-projects-page.component";
 import { WorkExperiencePageComponent } from "./work-experience-page/work-experience-page.component";
+import { Error404Component } from "./errors/components/error404/error404.component";
 
 const routes: Routes = [
   { path: "", component: HomePageComponent },
   { path: "creative-work", component: CreativeWorkPageComponent },
   { path: "code-projects", component: CodeProjectsPageComponent },
   { path: "work-experience", component: WorkExperiencePageComponent },
-  { path: "**", component: HomePageComponent },
+  { path: "**", component: Error404Component },
 ];
 
 @NgModule({

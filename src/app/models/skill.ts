0a47b2b1e3bfcdf6d3934e969cf63e5e02d28fde
Mike DelGaudio <mdelgaud@stevens.edu>
2020-08-22 16:44:51 -0400

    added more badges and auto scroller

--- src/app/models/skill.ts
+++ src/app/models/skill.ts
@@ -4,7 +4,14 @@ export interface Skill {
 }
 
 export const ListOfSkills: Skill[] = [
-  { skillName: "Angular", skillColor: "red" },
-  { skillName: "SCSS", skillColor: "purple" },
-  { skillName: "Javascript", skillColor: "#efdb4f" },
+  { skillName: "Angular", skillColor: "#be0e2d" },
+  { skillName: "HTML", skillColor: "#de4c26" },
+  { skillName: "SCSS", skillColor: "#c96395" },
+  { skillName: "Python", skillColor: "#336d9c" },
+  { skillName: "JavaScript", skillColor: "#dcc314" },
+  { skillName: "Bash", skillColor: "#2b3337" },
+  { skillName: "Docker", skillColor: "#0698e6" },
+  { skillName: "Node.js", skillColor: "#3d873d" },
+  { skillName: "Java", skillColor: "#5283a2" },
+  { skillName: "Bootstrap", skillColor: "#553b7c" },
 ];

d27ac84e8d40a006d79b000f90bc5f701ed32f98
Mike DelGaudio <mdelgaud@stevens.edu>
2020-09-12 19:36:57 -0400

    working on potentially using svgs for background

--- src/app/models/projects/code-projects.ts
+++ src/app/models/projects/code-projects.ts
@@ -11,7 +11,7 @@ export const ListOfCodeProjects: Project[] = [
   {
     heading: "DelGaudioMike_v2",
     description:
-      "Created a responsive website with roommate to access any given stock by pulling financial data from company SEC Filings. Through the use of an API, financial data is called for, then used a variety of calculations to produce a score.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur scelerisque eget velit vitae placerat. Phasellus tempus fermentum enim eget sagittis. Quisque eget eros ut lorem efficitur scelerisque. Nulla quis turpis sollicitudin, finibus ligula ac, rhoncus augue. Praesent at metus consectetur, pharetra eros sit amet, aliquam purus. Vivamus vitae enim sapien. Donec non consequat velit.",
     asset: "../../../assets/code-projects/desktop_bullish_bay.gif",
     assetAlt: "Bullish Bay Desktop View GIF",
     srcUrl: "https://github.com/mikedelgaudio/delgaudiomike.com_v2",
@@ -19,7 +19,7 @@ export const ListOfCodeProjects: Project[] = [
   {
     heading: "DelGaudioMike",
     description:
-      "Created a responsive website with roommate to access any given stock by pulling financial data from company SEC Filings. Through the use of an API, financial data is called for, then used a variety of calculations to produce a score.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur scelerisque eget velit vitae placerat. Phasellus tempus fermentum enim eget sagittis. Quisque eget eros ut lorem efficitur scelerisque. Nulla quis turpis sollicitudin, finibus ligula ac, rhoncus augue. Praesent at metus consectetur, pharetra eros sit amet, aliquam purus. Vivamus vitae enim sapien. Donec non consequat velit.",
     asset: "../../../assets/code-projects/desktop_bullish_bay.gif",
     assetAlt: "Bullish Bay Desktop View GIF",
     srcUrl: "https://github.com/mikedelgaudio/delgaudiomike.com",

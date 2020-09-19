b321a64e3e9a8efebed13140208bdb192a76587e
Mike DelGaudio <mdelgaud@stevens.edu>
2020-09-19 14:30:01 -0400

    adding creative work asset folder; moving files to respective directory

--- src/app/shared/experience-card/experience-card.component.ts
+++ src/app/shared/experience-card/experience-card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-experience-card',
-  templateUrl: './experience-card.component.html',
-  styleUrls: ['./experience-card.component.scss']
+  selector: "app-experience-card",
+  templateUrl: "./experience-card.component.html",
+  styleUrls: ["./experience-card.component.scss"],
 })
 export class ExperienceCardComponent implements OnInit {
+  @Input("heading") heading: string;
+  @Input("description") description: string;
+  @Input("asset") asset: string;
+  @Input("assetAlt") assetAlt: string;
+  @Input("bgColor") bgColor: string;
+  @Input("textColor") textColor: string;
+  @Input("startDate") startDate: string;
+  @Input("endDate") endDate: string;
+  @Input("location") location: string;
+  @Input("jobTitle") jobTitle: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

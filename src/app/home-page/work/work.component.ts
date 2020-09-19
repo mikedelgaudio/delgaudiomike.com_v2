66bf432dd933ae4661829776c9a0ed0fe224923b
Mike DelGaudio <mdelgaud@stevens.edu>
2020-09-19 13:48:44 -0400

    creating work experience shared component; changing background; fixing routes

--- src/app/home-page/work/work.component.ts
+++ src/app/home-page/work/work.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-work',
-  templateUrl: './work.component.html',
-  styleUrls: ['./work.component.scss']
+  selector: "app-work",
+  templateUrl: "./work.component.html",
+  styleUrls: ["./work.component.scss"],
 })
 export class WorkComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/work-experience-page/work-experience-page.module.ts
+++ src/app/work-experience-page/work-experience-page.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { WorkExperiencePageComponent } from "./work-experience-page.component";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [WorkExperiencePageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
+  exports: [WorkExperiencePageComponent],
 })
 export class WorkExperiencePageModule {}

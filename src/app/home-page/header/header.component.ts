4fb513ab97976dbe360c104b30c3cfe91ce110af
Mike DelGaudio <mdelgaud@stevens.edu>
2020-09-29 17:08:44 -0400

    adding smooth scroll

--- src/app/home-page/header/header.component.ts
+++ src/app/home-page/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, HostListener, Input } from "@angular/core";
+
+declare var $: any;
+
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
@@ -8,7 +11,37 @@ export class HeaderComponent implements OnInit {
   public scrolled: boolean = false;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getSmoothScroll();
+  }
+
+  /* SMOOTH SCROLLING */
+
+  get getSmoothScroll() {
+    return this.smoothScroll.bind(this);
+  }
+  public smoothScroll() {
+    const $root = $("html, body");
+
+    $(".scroller").click(() => {
+      $root.animate(
+        {
+          scrollTop: $("#about").offset().top,
+        },
+        800,
+        () => {
+          window.location.hash = "#about";
+        }
+      );
+    });
+
+    //Allow scroll animation interruption
+    $root.bind("scroll mousedown DOMMouseScroll mousewheel keyup touchstart", function (e) {
+      if (e.which > 0 || e.type === "mousedown" || e.type === "mousewheel" || e.type === "touchstart") {
+        $root.stop();
+      }
+    });
+  }
 
   @HostListener("window:scroll", ["$event"])
   onWindowScroll($event) {

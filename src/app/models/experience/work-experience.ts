cd6d28606b5e6f9f1a8c53b164cdbb6b6f1ac236
Mike DelGaudio <delgaudiomike@gmail.com>
2021-05-20 22:51:41 -0400

    adding new design cleanup

--- src/app/models/experience/work-experience.ts
+++ src/app/models/experience/work-experience.ts
@@ -2,36 +2,47 @@ import { Experience } from "./experience";
 
 export const ListOfWorkExperience: Experience[] = [
   {
-    heading: "Colgate-Palmolive Company",
-    location: "Piscataway, NJ",
-    startDate: "January 2020",
+    heading: "Google",
+    location: "Virtual / Remote",
+    startDate: "Jun 2021",
     endDate: "Present",
+    jobTitle: "Technical Writer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam venenatis metus et ex molestie dictum. Maecenas justo arcu, efficitur vitae pellentesque eu, varius vitae libero. Mauris lacinia, lacus in sagittis semper, tortor magna tincidunt ligula, sit amet eleifend nisi est eu lacus. Proin egestas mattis nisl sit amet tincidunt. Morbi eget neque eu lacus hendrerit interdum in at quam. Cras sed dapibus mauris. Aliquam at turpis sit amet turpis elementum varius.",
+    asset: "assets/work-experience/google.jpg",
+    assetAlt: "Code Backsplash",
+  },
+  {
+    heading: "Colgate Palmolive",
+    location: "Piscataway, NJ",
+    startDate: "Jan 2020",
+    endDate: "May 2021",
     jobTitle: "Application & Software Developer",
     description:
-      "I work apart of the Hill's B2B eCommerce frontend development team utilizing Angular 9 & SAP Spartacus components to develop numerous user interfaces with SCSS, HTML, and TypeScript. Additionally, I was able to research and develop solutions in the Selenium automated testing framework for the deployment of the project.",
-    asset: "../../../assets/work-experience/colgate.jpg",
+      "I worked with Hill's B2B eCommerce front-end development team utilizing Angular 9 & SAP Spartacus components to develop numerous user interfaces with SCSS, HTML, and TypeScript. Additionally, I was able to research and develop solutions in the Selenium automated testing framework for the deployment of the project.",
+    asset: "assets/work-experience/colgate.jpg",
     assetAlt: "Code Backsplash",
   },
   {
     heading: "Stevens Institute of Technology",
     location: "Hoboken, NJ",
-    startDate: "August 2019",
-    endDate: "Decemember 2019",
+    startDate: "Aug 2019",
+    endDate: "Dec 2019",
     jobTitle: "Peer Tutor",
     description:
       "Tutored numerous students for the Stevens Academic Support Center in computer science classes, providing me the opportunity to reinforce my knowledge in the field and public speaking skills.",
-    asset: "../../../assets/work-experience/stevens.jpg",
+    asset: "assets/work-experience/stevens.jpg",
     assetAlt: "Stevens Campus",
   },
   {
     heading: "U.S. SDNY Probation",
     location: "New York City, NY",
-    startDate: "June 2019",
-    endDate: "August 2019",
+    startDate: "Jun 2019",
+    endDate: "Aug 2019",
     jobTitle: "Web Developer",
     description:
       "I worked closely with the Division of Information Technology and the senior web developer on designing and developing two new full websites in PHP. Additionally, I was able to relocate and document all existing programming projects and in-house applications to a local git server for version control.",
-    asset: "../../../assets/work-experience/courts.jpg",
+    asset: "assets/work-experience/courts.jpg",
     assetAlt: "",
   },
 ];

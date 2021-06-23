608d0462acfd4c163113e2e6c1623a816677dbca
Mike DelGaudio <mdelgaud@stevens.edu>
2021-06-22 22:35:51 -0400

    start redesign of homepage

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { CallToActionButtonsComponent } from "./call-to-action-buttons/call-to-action-buttons.component";
 import { ProjectCardComponent } from "./project-card/project-card.component";
 import { ExperienceCardComponent } from "./experience-card/experience-card.component";
 
 @NgModule({
-  declarations: [CallToActionButtonsComponent, ProjectCardComponent, ExperienceCardComponent],
+  declarations: [ProjectCardComponent, ExperienceCardComponent],
   imports: [CommonModule],
-  exports: [CallToActionButtonsComponent, ProjectCardComponent, ExperienceCardComponent],
+  exports: [ProjectCardComponent, ExperienceCardComponent],
 })
 export class SharedModule {}

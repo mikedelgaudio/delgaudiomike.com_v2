d04b3a38342bf0947420ee441519a97091790516
Mike DelGaudio <mdelgaud@stevens.edu>
2021-07-08 23:01:48 -0400

    changing assets

--- src/app/models/projects/code-projects.ts
+++ src/app/models/projects/code-projects.ts
@@ -2,6 +2,14 @@ import { Project } from "./project";
 
 export const ListOfCodeProjects: Project[] = [
   {
+    heading: "Bullish Bay Chatbot",
+    description:
+      "With the use of NodeJS, Discord's public API and Discord's NPM package, I created a chatbot users can interact with in multiple Discord servers. The bot can provide the latest news stories on Wall Street or display the current price information based on a company stock ticker. The application is containerized using Docker and deploys automatically to my personal hypervisor.",
+    asset: "../../../assets/home/projects/bbay-discord.jpg",
+    assetAlt: "Bullish Bay Discord Chat Bot Demo GIF",
+    srcUrl: "https://github.com/mikedelgaudio/Bullish-Bay-Discord-Bot",
+  },
+  {
     heading: "WeatherFlash",
     description:
       "This project is based purely on the technical stack of NextJS, React, and React Hooks. Utilzing an external API and the browser's geolocation, the app will give a minimalistic snapshot of the current weather conditions in your area. The application is containerized using Docker and deploys automatically to my personal hypervisor.",
@@ -17,13 +25,5 @@ export const ListOfCodeProjects: Project[] = [
     asset: "../../../assets/home/projects/bbay.jpg",
     assetAlt: "Bullish Bay Desktop View GIF",
     demoUrl: "https://bullishbay.delgaudiomike.com",
-  },
-  {
-    heading: "Bullish Bay Chatbot",
-    description:
-      "With the use of NodeJS, Discord's public API and Discord's NPM package, I created a chatbot users can interact with in multiple Discord servers. The bot can provide the latest news stories on Wall Street or display the current price information based on a company stock ticker. The application is containerized using Docker and deploys automatically to my personal hypervisor.",
-    asset: "../../../assets/home/projects/bbay.jpg",
-    assetAlt: "Bullish Bay Discord Chat Bot Demo GIF",
-    srcUrl: "https://github.com/mikedelgaudio/Bullish-Bay-Discord-Bot",
   },
 ];

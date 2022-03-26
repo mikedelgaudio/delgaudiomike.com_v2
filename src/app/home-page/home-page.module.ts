f2108e0065abbec8a378a1a0caac45a1f1058889
Mike DelGaudio <delgaudiomike@gmail.com>
2022-03-25 21:18:55 -0400

    adding spacing between homepage components and rendering svg icons

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { HomePageComponent } from "./home-page.component";
-import { WorkModule } from "./work/work.module";
-import { HeaderModule } from "./header/header.module";
-import { AboutModule } from "./about/about.module";
-import { ContactModule } from "./contact/contact.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './home-page.component';
+import { WorkModule } from './work/work.module';
+import { HeaderModule } from './header/header.module';
+import { AboutModule } from './about/about.module';
+import { ContactModule } from './contact/contact.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, ContactModule, AboutModule, WorkModule, HeaderModule],
+  imports: [CommonModule, ContactModule, AboutModule, WorkModule, HeaderModule, SharedModule],
   exports: [],
 })
 export class HomePageModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EnterTheViewportNotifierDirective } from './enter-viewport.directive';
+import { SpacerComponent } from './spacer/spacer.component';
 
 @NgModule({
-  declarations: [EnterTheViewportNotifierDirective],
+  declarations: [EnterTheViewportNotifierDirective, SpacerComponent],
   imports: [CommonModule],
-  exports: [EnterTheViewportNotifierDirective],
+  exports: [EnterTheViewportNotifierDirective, SpacerComponent],
 })
 export class SharedModule {}
